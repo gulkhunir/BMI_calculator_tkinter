5ba0dd5dbf4c39f4882959a9b43767ddab6b3a93
Mehmet GÜVEN <guvennmehmet@gmail.com>
2023-06-13 02:44:17 +0300

    bug fixed

--- main.py
+++ main.py
@@ -6,26 +6,32 @@ window.minsize(width=300, height=150)
 
 
 def calc_bmi():
-    w_input = int(wei_entry.get())
-    h_input = int(hei_entry.get())
-    bmi = w_input / (h_input * h_input/10000)
     classification = ["Under Weight", "Normal", "Over Weight", "Obesity (Class I)", "Obesity (Class II)",
                       "Extreme Obesity"]
 
-    for i in range(len(classification)):
-        if bmi < 18.5:
-            i = 0
-        elif bmi < 24.9:
-            i = 1
-        elif bmi < 29.9:
-            i = 2
-        elif bmi < 34.9:
-            i = 3
-        elif bmi < 39.9:
-            i = 4
-        elif bmi > 40:
-            i = 5
-    sonuc_label.config(text=("Your Bmi is " + '{:.2f}'.format(bmi) + ". You are " + classification[i]))
+    w_input = wei_entry.get()
+    h_input = hei_entry.get()
+    if w_input == "" or h_input == "":
+        result_label.config(text="Enter both weight and height")
+    else:
+        try:
+            bmi = (float(w_input) / (float(h_input) / 100) ** 2)
+            for i in range(len(classification)):
+                if bmi < 18.5:
+                    i = 0
+                elif bmi < 24.9:
+                    i = 1
+                elif bmi < 29.9:
+                    i = 2
+                elif bmi < 34.9:
+                    i = 3
+                elif bmi < 39.9:
+                    i = 4
+                elif bmi > 40:
+                    i = 5
+            result_label.config(text=f"Your Bmi is {round(bmi,2)}. You are {classification[i]}!")
+        except:
+            result_label.config(text="Enter a valid number")
 
 
 wei_label = tkinter.Label(text="Enter Your Weight (kg)")
@@ -38,7 +44,7 @@ hei_entry = tkinter.Entry(width=10)
 hei_entry.pack()
 calc_button = tkinter.Button(text="Calculate", command=calc_bmi)
 calc_button.pack()
-sonuc_label = tkinter.Label()
-sonuc_label.pack()
+result_label = tkinter.Label()
+result_label.pack()
 
 window.mainloop()
